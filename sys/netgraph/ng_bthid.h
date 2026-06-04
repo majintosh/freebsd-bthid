af3849b9a1f6c0b7d6c71a4a3c714d6491831333
Majed Alkhaleefah <majed@freebsd.org>
2026-06-04 17:20:19 +0000

    Refactor ng_bthid.h

---
+++ sys/netgraph/ng_bthid.h
@@ -0,0 +1,12 @@
+/*
+ * ng_bthid.h
+ */
+
+#ifndef _NETGRAPH_NG_BTHID_H_
+#define _NETGRAPH_NG_BTHID_H_
+
+/* Node type name and magic cookie */
+#define NG_BTHID_NODE_TYPE	"bthid"
+#define NGM_BTHID_COOKIE	1780544700
+
+#endif /* _NETGRAPH_NG_BTHID_H_ */
