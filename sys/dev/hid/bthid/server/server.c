e6ad6577430a5150e34810344870b0a7c7ae4380
Majed Alkhaleefah <majed@freebsd.org>
2026-06-21 22:15:22 +0000

    Add worker thread to print packets

--- sys/dev/hid/bthid/server/server.c
+++ sys/dev/hid/bthid/server/server.c
@@ -6,6 +6,10 @@
 #include <sys/module.h>
 #include <sys/proc.h>
 #include <sys/systm.h>
+#include <sys/uio.h>
+#include <sys/mutex.h>
+#include <sys/taskqueue.h>
+#include <sys/mbuf.h>
 
 #include <netgraph/bluetooth/include/ng_bluetooth.h>
 #include <netgraph/bluetooth/include/ng_hci.h>
@@ -13,13 +17,41 @@
 #include <netgraph/bluetooth/include/ng_btsocket.h>
 
 static struct socket *ctrl, *intr;
+static struct task printer_task;
+
+static void
+printer(void *context, int pending)
+{
+	struct socket *s = (struct socket*) context;
+	int flag = MSG_DONTWAIT;
+	struct uio thing;
+	thing.uio_resid = 1000000;
+	thing.uio_td = curthread;
+	struct mbuf *m = NULL;
+	soreceive(s, NULL, &thing, &m, NULL, &flag);
+	if (m != NULL) {
+		uint8_t* payload = mtod(m, uint8_t *);
+		for (int i = 0; i<min(7, m->m_len/sizeof(uint8_t)); i++)
+		{	
+			printf("%02X ", payload[i]);
+		}
+		printf("\n");
+	}
+}
 
 static int
-socket_setup(struct socket *sock, uint16_t psm)
+soup(struct socket *s, void *arg, int which)
+{
+	taskqueue_enqueue(taskqueue_thread, &printer_task);
+	return SU_OK;
+}
+
+static int
+socket_setup(struct socket **sock, uint16_t psm)
 {
 	struct sockaddr_l2cap l2addr;
 	int error = 0;
-	error = socreate(PF_BLUETOOTH, &sock, SOCK_SEQPACKET,
+	error = socreate(PF_BLUETOOTH, sock, SOCK_SEQPACKET,
 	    BLUETOOTH_PROTO_L2CAP, curthread->td_ucred, curthread);
 	if (error != 0) {
 		printf("We couldn't create the socket!\n");
@@ -34,7 +66,7 @@ socket_setup(struct socket *sock, uint16_t psm)
 	l2addr.l2cap_bdaddr_type = BDADDR_BREDR;
 	l2addr.l2cap_cid = 0;
 
-	error = sobind(sock, (struct sockaddr *)&l2addr, curthread);
+	error = sobind(*sock, (struct sockaddr *)&l2addr, curthread);
 
 	if (error != 0) {
 		printf("We couldn't bind the socket!\n");
@@ -45,49 +77,26 @@ socket_setup(struct socket *sock, uint16_t psm)
 	
 	l2addr.l2cap_psm = htole16(psm); 
 
-	bdaddr_t controller_addr = {{0xMA, 0xJE, 0xDW, 0xAS, 0xHE, 0xRE}}; // Replace this with the actual bdaddr. Needs to be in little endian format.
+	bdaddr_t controller_addr = {{0x00, 0x11, 0x22, 0x33, 0x44, 0x55}};
 
 	memcpy(&l2addr.l2cap_bdaddr, &controller_addr, sizeof(l2addr.l2cap_bdaddr));
 
-	error = soconnect(sock, (struct sockaddr *) &l2addr, curthread);
+	error = soconnect(*sock, (struct sockaddr *) &l2addr, curthread);
 
 	if (error != 0) {
 		printf("We couldn't connect the socket!\n");
 		goto cleanup;
 	}
 
-#if 0
-	error = solisten(sock, 10, curthread);
+	SOCK_RECVBUF_LOCK(*sock);
+	soupcall_set(*sock, SO_RCV, soup, NULL); // soup, like the food.
+	SOCK_RECVBUF_UNLOCK(*sock);
 
-	if (error != 0) {
-		printf("We couldn't listen on the socket!\n");
-		soclose(sock);
-		sock = NULL;
-		return error;
-	}
-
-	printf("We're listening on the socket!\n");
-
-	error = soaccept(sock, (struct sockaddr*) &l2addr);
-
-	if (error != 0) {
-		printf("We couldn't accept the socket!\n");
-		soclose(sock);
-		sock = NULL;
-		return error;
-	}
-
-
-	sock->so_linger = 1;
-	printf("We accepted the socket!\n");
-	printf("Linger is: %d\n", sock->so_linger);
-	printf("Addr is: %p\n", sock);
-#endif
 	return error;
 
-cleanup:
-	soclose(sock);
-	sock = NULL;
+cleanup: // Get rid of the label and goto stuff
+	soclose(*sock);
+	*sock = NULL;
 	return error;
 }
 
@@ -95,8 +104,10 @@ static void
 socket_close(struct socket *sock)
 {
 	if (sock != NULL) {
+		SOCK_RECVBUF_LOCK(sock);
+		soupcall_clear(sock, SO_RCV);
+		SOCK_RECVBUF_UNLOCK(sock);
 		soclose(sock);
-		sock = NULL;
 		printf("We closed the socket!\n");
 	}
 }
@@ -109,12 +120,15 @@ bthidbus_server_modevent(module_t mod, int type, void *data)
 	switch (type) {
 		
 		case MOD_LOAD:
-			socket_setup(ctrl, 0x11);
-			socket_setup(intr, 0x13);
+			socket_setup(&ctrl, 0x11);
+			socket_setup(&intr, 0x13);
+			TASK_INIT(&printer_task, 0, printer, intr); // Worried this might run after our intr socket already receives a packet
 			break;
 		case MOD_UNLOAD:
 			socket_close(ctrl);
+			ctrl = NULL;
 			socket_close(intr);
+			intr = NULL;
 			break;
 		default:
 			break;
