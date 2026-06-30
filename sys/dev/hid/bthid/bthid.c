b49f304f0d3e3c94972b0d698ce68b23d380f919
Majed Alkhaleefah <majed@freebsd.org>
2026-06-30 16:43:47 +0300

    Check if upcall exists before clearing it

--- sys/dev/hid/bthid/bthid.c
+++ sys/dev/hid/bthid/bthid.c
@@ -44,7 +44,8 @@ socket_close(struct socket* sock)
 	if (sock == NULL)
 		return 0;
 	SOCK_RECVBUF_LOCK(sock);
-	soupcall_clear(sock, SO_RCV);
+	if (sock->so_rcv.sb_upcall != NULL)
+		soupcall_clear(sock, SO_RCV);
 	SOCK_RECVBUF_UNLOCK(sock);
 	soclose(sock);
 	return 0;
@@ -107,7 +108,6 @@ static int
 bthid_intr_start(device_t dev, device_t child __unused)
 {
 	struct bthid_softc *sc = device_get_softc(dev);
-
 	SOCK_RECVBUF_LOCK(sc->intr);
 	soupcall_set(sc->intr, SO_RCV, intr_upcall, sc->intr_task);
 	SOCK_RECVBUF_UNLOCK(sc->intr);
