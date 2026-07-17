264274396a621003c47aa6f3da311854a2405040
Majed Alkhaleefah <majed@freebsd.org>
2026-07-17 02:32:10 +0000

    Add additional fields.

--- sys/dev/hid/bthid/bthidbus.h
+++ sys/dev/hid/bthid/bthidbus.h
@@ -1,8 +1,15 @@
 #include <sys/ioccom.h>
+#include <sys/types.h>
 
 struct bthidbus_new_connection {
 	int ctrl_sock;
 	int intr_sock;
+	uint16_t vendorId;
+	uint16_t productId;
+	uint16_t versionId;
+	void *rdesc;
+	size_t rdesc_len;
+
 };
 
 #define BTHIDBUS_NEW_CONNECTION _IOW('B', 1, struct bthidbus_new_connection)
