d6852941c382a820353e52c1aefdbc9a4c06ac74
Majed Alkhaleefah <majed@freebsd.org>
2026-07-19 20:41:06 +0300

    Add file pointer fields to bthid ivars

--- sys/dev/hid/bthid/bthid.h
+++ sys/dev/hid/bthid/bthid.h
@@ -1,9 +1,12 @@
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/file.h>
 
 struct bthid_ivars {
 	struct socket* ctrl_sock;
 	struct socket* intr_sock;
+	struct file* ctrl_file;
+	struct file* intr_file;
 	uint16_t vendorId;
 	uint16_t productId;
 	uint16_t versionId;
